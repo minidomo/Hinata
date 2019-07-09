ae208f52a3c9bdfbf8800fce23ea1c7b6ac6a0a9
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2019-07-09 14:10:40 -0500

    removed comments in logger and adjusted github url

--- util/logger.js
+++ util/logger.js
@@ -13,22 +13,4 @@ const Logger = winston.createLogger({
     format: winston.format.printf(log => `[${log.level.toUpperCase()} | ${timestamp()}] - ${log.message}`)
 });
 
-// Logger.log = args => {
-//     console.log(`${timestamp()} ${args}`);
-// };
-
-// Logger.objectLog = args => {
-//     console.log(timestamp());
-//     console.log(args);
-// };
-
-// Logger.error = args => {
-//     console.error(`${timestamp()} ${args}`)
-// };
-
-// Logger.realError = args => {
-//     console.log(timestamp());
-//     console.error(args);
-// };
-
 module.exports = Logger;
